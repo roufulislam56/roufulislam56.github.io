4e18ca70c28a38573fc40a2bb3df7a0a2f15ec37
roufulislam56 <roufulislam56@gmail.com>
2026-07-04 13:16:00 +0600

    Add product filtering and search functionality

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Easy Shop Loaded Successfully!");
 });
+function showAll(){
+
+document.querySelectorAll(".product").forEach(function(item){
+
+item.style.display="block";
+
+});
+
+}
+
+function showMen(){
+
+document.querySelectorAll(".product").forEach(function(item){
+
+item.style.display="none";
+
+});
+
+document.querySelectorAll(".men").forEach(function(item){
+
+item.style.display="block";
+
+});
+
+}
+
+function showWomen(){
+
+document.querySelectorAll(".product").forEach(function(item){
+
+item.style.display="none";
+
+});
+
+document.querySelectorAll(".women").forEach(function(item){
+
+item.style.display="block";
+
+});
+
+}
+const search = document.getElementById("searchInput");
+
+search.addEventListener("keyup", function(){
+
+let value = search.value.toLowerCase();
+
+document.querySelectorAll(".product").forEach(function(product){
+
+let text = product.innerText.toLowerCase();
+
+product.style.display = text.includes(value) ? "block" : "none";
+
+});
+
+});
